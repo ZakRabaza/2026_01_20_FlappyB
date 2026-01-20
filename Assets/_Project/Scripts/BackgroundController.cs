341c83740057565e362e4580847504666ae738fb
Rabaza <zak.rabaza@gmail.com>
2026-01-20 15:19:14 +0100

    feat: refactoring code backgroundController script

--- Assets/_Project/Scripts/BackgroundController.cs
+++ Assets/_Project/Scripts/BackgroundController.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class BackgroundController : MonoBehaviour
 {
     [SerializeField]
-    [Tooltip("This is the of the background")]
+    [Tooltip("This is the move speed of the background")]
     private float _moveSpeed = 2f;
 
     void Update()
     {
-        Vector2 translation = _moveSpeed * Time.deltaTime * Vector2.left;
+        MoveBackground();
+    }
 
+    void MoveBackground()
+    {
+        Vector2 translation = _moveSpeed * Time.deltaTime * Vector2.left;
         if (transform.position.x > -16)
             transform.Translate(translation);
         else
