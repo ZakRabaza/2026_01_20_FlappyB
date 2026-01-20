1f771da48e77bf5286183e8c4d69e866651d197b
Rabaza <zak.rabaza@gmail.com>
2026-01-20 14:35:47 +0100

    feat: refactoring code background script

--- Assets/_Project/Scripts/BackgroundController.cs
+++ Assets/_Project/Scripts/BackgroundController.cs
@@ -5,11 +5,13 @@ public class BackgroundController : MonoBehaviour
     [SerializeField]
     [Tooltip("This is the of the background")]
     private float _moveSpeed = 2f;
-    
+
     void Update()
     {
+        Vector2 translation = _moveSpeed * Time.deltaTime * Vector2.left;
+
         if (transform.position.x > -16)
-            transform.Translate(Vector2.left * _moveSpeed * Time.deltaTime);
+            transform.Translate(translation);
         else
         {
             transform.position = Vector2.zero;
