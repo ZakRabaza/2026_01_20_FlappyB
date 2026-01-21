c014d647ec42f3be3b911bcb5c047e2dba0344ae
Rabaza <zak.rabaza@gmail.com>
2026-01-21 13:26:23 +0100

    feat: adding pipe in the game , the height is generated and the gap can be changed

--- Assets/_Project/Scripts/PipeController.cs
+++ Assets/_Project/Scripts/PipeController.cs
@@ -2,24 +2,73 @@ using UnityEngine;
 
 public class PipeController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Pipe Settings")]
+
+    [Range(1f, 10f)]
+    [SerializeField]
+    [Tooltip("This is the move speed of the pipes")]
+    private float _moveSpeed = 2f;
+
+    [Tooltip("This is the gap between the pipe")]
+    [SerializeField] 
+    private float _gap = 4f;
+
+    [Tooltip("This is the minimum height of center between the pipe , space included")]
+    [SerializeField] 
+    private float minCenterY = -6f;
+
+    [Tooltip("This is the maximum height of center between the pipe , space included")]
+    [SerializeField] 
+    private float maxCenterY = 6f;
+
+    private Transform _pipeUp;
+    private Transform _pipeDown;
+
     void Start()
     {
-        
+        SetInitPositions();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovePipe();
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    void SetInitPositions()
     {
-        Debug.Log("Bird Dead RIP");
+        _pipeUp = transform.Find("PipeUp");
+        _pipeDown = transform.Find("PipeDown");
+
+        _pipeUp.localPosition = new Vector2(0, -2f);
+        _pipeDown.localPosition = new Vector2(0, 2f);    
     }
-    public void OnTriggerEnter2D(Collider2D collision)
+
+    void MovePipe()
+{
+        Vector2 translation = _moveSpeed * Time.deltaTime * Vector2.left;
+        if (transform.position.x > -16)
+            transform.Translate(translation);
+        else
+        {
+            transform.position = Vector2.zero;
+            EditPipeAttributes();
+        }
+    }
+    void EditPipeAttributes()
     {
-        Debug.Log("Bird Dead RIP");
+        float centerY = Random.Range(minCenterY, maxCenterY);
+    
+        _pipeUp.localPosition = new Vector2(0, centerY - _gap / 2f);
+        _pipeDown.localPosition = new Vector2(0, centerY + _gap / 2f);
+
     }
+
+    //public void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("Bird Dead RIP");
+    //}
+    //public void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Debug.Log("Bird Dead RIP");
+    //}
 }
